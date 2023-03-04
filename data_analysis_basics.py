60ff059642d9b348ee49f59feb04cb91adfbf7e9
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-03-04 22:49:03 +0100

    Update data_analysis_basics.py

--- data_analysis_basics.py
+++ data_analysis_basics.py
@@ -12,3 +12,11 @@ print(data.describe())
 # Group data by category and calculate average value of numeric columns
 grouped_data = data.groupby('category').mean()
 print(grouped_data)
+
+
+#In this example, we first import the Pandas library using the import statement. We then read in a CSV file called data.csv using the pd.read_csv() 
+#function and assign it to a variable called data. We then use the head() method to display the first 5 rows of data and the describe() method to display 
+# summary statistics for the numerical columns in the dataset.
+
+#Finally, we group the data by category using the groupby() method and calculate the average value of the numeric columns using the mean() method. 
+#The resulting grouped data is then printed to the console.
